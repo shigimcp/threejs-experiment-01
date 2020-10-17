1a437580a0b86f111ccfa793480a3b548f92c06c
shigimcp <shigimcp@hotmail.com>
2020-10-17 04:20:42 -0400

    fonts

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,8 +1,15 @@
 //#region ==================== IMPORTS ====================
 
-import React from "react";
+import React, { useState } from "react";
 
 import { DoubleSide } from "three";
+
+import { extend } from "react-three-fiber";
+
+import { Text } from 'troika-three-text';
+import fonts from "../fonts/fonts";
+extend({ Text });
+
 
 //#endregion ==================== IMPORTS ====================
 
@@ -42,9 +49,44 @@ export default () => {
     //#endregion ==================== SCENE - REF: https://codeworkshop.dev/blog/2019-12-31-learn-the-basics-of-react-three-fiber-by-building-a-three-point-lighting-setup/ ====================
 
 
+    //#region ==================== TEXT - REF: https://www.npmjs.com/package/troika-three-text ====================
+    //        ==================== TEXT - REF: https://codesandbox.io/s/troika-3d-text-via-react-three-fiber-forked-1ebyu ====================
+
+    const text = "Shigeru McPherson";
+
+    const [rotation, setRotation] = useState([0, 0, 0, 0]);
+
+    const [opts, setOpts] = useState({
+        font: "AnimeAceItalic",
+        fontSize: 1.5,
+        color: "#99ccff",
+        maxWidth: 20,
+        lineHeight: 1,
+        letterSpacing: 0,
+        textAlign: "left",
+        materialType: "MeshPhongMaterial"
+    });
+
+    //#endregion ==================== TEXT - REF: https://www.npmjs.com/package/troika-three-text ====================
+
+
 
     return (
         <>
+            <text
+                position-z={-5}
+                rotation={rotation}
+                {...opts}
+                text={text}
+                font={fonts[opts.font]}
+                anchorX="center"
+                anchorY="middle"
+            >
+                {opts.materialType === "MeshPhongMaterial" ? (
+                    <meshPhongMaterial attach="material" color={opts.color} />
+                ) : null}
+            </text>
+
             <BackDrop />
             <GroundPlane />
         </>
