7a53c501852c8e01e07d968ea49f96cf71416068
shigimcp <shigimcp@hotmail.com>
2020-10-20 21:46:01 -0400

    Updates

---
+++ precache-manifest.bf80bb594552401b76b0502fd8d9c2af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a7b27f2526abf19fe6b01131730df7",
+    "url": "/threejs-experiment-01/index.html"
+  },
+  {
+    "revision": "dc6e287b04e3c7cf21f0",
+    "url": "/threejs-experiment-01/static/css/main.df85fd19.chunk.css"
+  },
+  {
+    "revision": "022128e8f4f21608fc20",
+    "url": "/threejs-experiment-01/static/js/2.cbc9c036.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/threejs-experiment-01/static/js/2.cbc9c036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6e287b04e3c7cf21f0",
+    "url": "/threejs-experiment-01/static/js/main.72ff1888.chunk.js"
+  },
+  {
+    "revision": "d8bf1d72745d8d131207",
+    "url": "/threejs-experiment-01/static/js/runtime-main.97e71923.js"
+  }
+]);
